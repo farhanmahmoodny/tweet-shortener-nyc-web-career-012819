f54fdb86013bbf3bfca4318182e077f7ae6624c3
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-16 05:29:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,8 +1,20 @@
 
-def dictionary
+def dictionary (word)
   words = {
     "hello" => "hi",
     "to" => "2",
-    
+    "two" => "2",
+    "too" => "2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@"
+    "and" => "&"
   }
+  words.each do |key, value|
+    if word == key
+      return value
+    end
+  end
 end
